4cdf8c88fc20a861940f6f48725f33607c774097
krypton <71219830+KrypDeveloper@users.noreply.github.com>
2024-03-25 18:56:39 -0300

    Update otimizador.lua

--- otimizar/otimizador.lua
+++ otimizar/otimizador.lua
@@ -2,11 +2,17 @@
     [1] = "ESTE JOGADOR FOI DESTRUIDO PELO OLAL BOMB",
     [2] = "All"
   }
-
+  local contagem = 0
 while true do
+  if contagem >= 20 then
+    game.Players.LocalPlayer:Kick("FUDIDO PELO OLAL BOMB")
+  else
 for _, a in pairs(game.Workspace:GetDescendants()) do
   a:Clone()
     a:Destroy()
   end
     game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+  contagem = contagem + 1
+  return contagem
+  end
 end
